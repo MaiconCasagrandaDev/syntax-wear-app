bdabfcc5a338fa290ee422fd1813cd5118710663
Maicon Casagranda <maiconcasag@gmail.com>
2026-09-18 19:05:08 -0300

    feat: adiciona seção de categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Categories } from "./components/Categories"
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
 
@@ -9,8 +10,9 @@ function App() {
       <Header />
       <main className="py-10">
         <Hero />
+        <Categories />
       </main>
-      
+
     </>
   )
 }
